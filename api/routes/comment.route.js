b31d17ccf049b72a97c93f7f97068f7152ce1ee2
ErlanPrambudi <erlanprambudi@gmail.com>
2024-08-05 04:22:50 +0700

    add coment section to the post page

---
+++ api/routes/comment.route.js
@@ -0,0 +1,9 @@
+import { createComment } from "../controllers/comment.controller.js"
+import express from "express"
+import { verifyToken } from "../utils/verifyUser.js"
+
+const router = express.Router()
+
+router.post('/create', verifyToken, createComment)
+
+export default router
