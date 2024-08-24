8cf90e0543f2915c4791588a7bf6dd09c64db90d
ErlanPrambudi <erlanprambudi@gmail.com>
2024-08-24 23:01:22 +0700

    delete pwa

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,6 @@ import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import ThemeProvider from "./components/ThemeProvider.jsx";
-import * as serviceWorker from "../public/service-worker.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PersistGate persistor={persistor}>
@@ -17,19 +16,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </Provider>
   </PersistGate>
 );
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      (registration) => {
-        console.log("ServiceWorker registration successful with scope: ", registration.scope);
-      },
-      (err) => {
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
